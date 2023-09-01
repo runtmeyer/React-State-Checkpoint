9a82cf6b152a542e3123703e2a33045268bbac2f
runtmeyer <muobotone@gmail.com>
2023-09-01 12:20:38 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      person: {
+        fullName: 'Abraham Lincoln',
+        bio: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."',
+        imgSrc: 'https://ideapod.com/wp-content/uploads/2023/05/personality-traits-that-show-youre-a-smart-man-1152x605.jpg',
+        profession: 'Software Engineer',
+      },
+      show: false,
+      timeInterval: 0,
+    };
+  }
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState({ timeInterval: this.state.timeInterval + 1 });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  toggleShow = () => {
+    this.setState({ show: !this.state.show });
+  };
+
+  render() {
+    const { fullName, bio, imgSrc, profession } = this.state.person;
+    return (
+      <div className="App">
+        <button onClick={this.toggleShow}>
+          Toggle Profile
+        </button>
+        {this.state.show && (
+          <div>
+            <img src={imgSrc} alt={fullName} />
+            <h1>{fullName}</h1>
+            <p>{bio}</p>
+            <p>{profession}</p>
+          </div>
+        )}
+        <p style={{color: 'pink', }}>Time Interval since Mount: {this.state.timeInterval} seconds</p>
+      </div>
+    );
+  }
 }
 
 export default App;
